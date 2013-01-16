7b7804286dd191aaa862c9210772a28f68140b37
Michael Zanetti <michael_zanetti@gmx.net>
2013-01-16 17:34:58 +0100

    follow gnu recommendation for license headers.

--- tests/unittests/tst_QmlTests.cpp
+++ tests/unittests/tst_QmlTests.cpp
@@ -1,7 +1,9 @@
 /*
  * Copyright 2013 Canonical Ltd.
  *
- * This program is free software; you can redistribute it and/or modify
+ * This file is part of ubuntu-browser.
+ *
+ * ubuntu-browser is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; version 3.
  *
