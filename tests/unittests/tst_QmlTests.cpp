857de3dba62d3735d491cbcb976e16ae7ec1cab2
Michael Zanetti <michael_zanetti@gmx.net>
2013-01-16 17:45:41 +0100

    follow the gnu recommendation even closer

--- tests/unittests/tst_QmlTests.cpp
+++ tests/unittests/tst_QmlTests.cpp
@@ -7,7 +7,7 @@
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; version 3.
  *
- * This program is distributed in the hope that it will be useful,
+ * ubuntu-browser is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
