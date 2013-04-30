036c05b7553773e28957e54fc114e727880bebd3
Olivier Tilloy <olivier.tilloy@canonical.com>
2013-04-30 15:34:15 -0700

    Separate the toolbar tests into their own module.

--- tests/autopilot/webbrowser_app/tests/test_mainwindow.py
+++ tests/autopilot/webbrowser_app/tests/test_mainwindow.py
@@ -23,68 +23,6 @@ import time
 LOREMIPSUM = "<p>Lorem ipsum dolor sit amet.</p>"
 
 
-class TestMainWindowToolbar(BrowserTestCaseBase):
-
-    """Tests interaction with the toolbar."""
-
-    def test_reveal_chrome(self):
-        self.assert_chrome_hidden()
-        self.reveal_chrome()
-        self.assert_chrome_eventually_shown()
-
-    def test_reveal_chrome_with_partial_swipe(self):
-        self.assert_chrome_hidden()
-        self.swipe_chrome_up(10)
-        self.assert_chrome_eventually_shown()
-
-    def test_reveal_chrome_with_long_swipe(self):
-        chrome = self.main_window.get_chrome()
-        self.assert_chrome_hidden()
-        self.swipe_chrome_up(chrome.height * 2)
-        self.assert_chrome_eventually_shown()
-
-    def test_hide_chrome(self):
-        self.reveal_chrome()
-        self.hide_chrome()
-        self.assert_chrome_eventually_hidden()
-
-    def test_hide_chrome_with_partial_swipe(self):
-        self.reveal_chrome()
-        self.swipe_chrome_down(10)
-        self.assert_chrome_eventually_hidden()
-
-    def test_hide_chrome_with_long_swipe(self):
-        chrome = self.main_window.get_chrome()
-        self.reveal_chrome()
-        self.swipe_chrome_down(chrome.height * 2)
-        self.assert_chrome_eventually_hidden()
-
-    def test_unfocus_chrome_hides_it(self):
-        webview = self.main_window.get_web_view()
-        self.reveal_chrome()
-        self.mouse.move_to_object(webview)
-        self.mouse.click()
-        self.assert_chrome_eventually_hidden()
-
-    def test_swipe_down_hidden_chrome_doesnt_reveal_it(self):
-        view = self.main_window.get_qml_view()
-        x_line = int(view.x + view.width * 0.5)
-        start_y = int(view.y + view.height - 1)
-        stop_y = start_y + 20
-        self.mouse.drag(x_line, start_y, x_line, stop_y)
-        self.assert_chrome_eventually_hidden()
-
-    def test_swipe_shown_chrome_up_doesnt_hide_it(self):
-        view = self.main_window.get_qml_view()
-        chrome = self.main_window.get_chrome()
-        self.reveal_chrome()
-        x_line = int(view.x + view.width * 0.5)
-        start_y = int(chrome.globalRect[1])
-        stop_y = int(view.y - 1)
-        self.mouse.drag(x_line, start_y, x_line, stop_y)
-        self.assert_chrome_eventually_shown()
-
-
 class TestMainWindowTitle(BrowserTestCaseBase):
 
     """Tests that the window’s title reflects the page title."""
