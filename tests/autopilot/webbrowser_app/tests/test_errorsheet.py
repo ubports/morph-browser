14e5f4cc8d75c1d3666a21b9aa26ec72626be303
Olivier Tilloy <olivier.tilloy@canonical.com>
2013-05-14 16:36:11 +0200

    Ensure the autopilot tests pass even behind a proxy.

--- tests/autopilot/webbrowser_app/tests/test_errorsheet.py
+++ tests/autopilot/webbrowser_app/tests/test_errorsheet.py
@@ -21,5 +21,5 @@ class TestErrorSheet(StartOpenLocalPageTestCaseBase):
     def test_invalid_url_triggers_error_message(self):
         error = self.main_window.get_error_sheet()
         self.assertThat(error.visible, Equals(False))
-        self.go_to_url("http://invalid")
+        self.go_to_url("htpp://invalid")
         self.assertThat(error.visible, Eventually(Equals(True)))
