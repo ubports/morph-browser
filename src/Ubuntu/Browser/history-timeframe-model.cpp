cb42129db8c5805ad12b04d3c52a75d086fe319f
Olivier Tilloy <olivier.tilloy@canonical.com>
2013-06-18 10:20:44 +0200

    HistoryTimeframeModel and corresponding unit tests.

---
+++ src/Ubuntu/Browser/history-timeframe-model.cpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2013 Canonical Ltd.
+ *
+ * This file is part of webbrowser-app.
+ *
+ * webbrowser-app is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * webbrowser-app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "history-timeframe-model.h"
+#include "history-model.h"
+
+/*!
+    \class HistoryTimeframeModel
+    \brief Proxy model that filters the contents of the history model
+           excluding all entries that are not contained in a given timeframe
+
+    HistoryTimeframeModel is a proxy model that filters the contents of a
+    HistoryModel, excluding all entries that are not contained in a given
+    timeframe specified by a start datetime and an end datetime.
+
+    To leave one side of the timeframe open, do not set either the start or end
+    datetime (or reset them to an invalid datetime).
+*/
+HistoryTimeframeModel::HistoryTimeframeModel(QObject* parent)
+    : QSortFilterProxyModel(parent)
+{
+}
+
+HistoryModel* HistoryTimeframeModel::sourceModel() const
+{
+    return qobject_cast<HistoryModel*>(QSortFilterProxyModel::sourceModel());
+}
+
+void HistoryTimeframeModel::setSourceModel(HistoryModel* sourceModel)
+{
+    if (sourceModel != this->sourceModel()) {
+        QSortFilterProxyModel::setSourceModel(sourceModel);
+        Q_EMIT sourceModelChanged();
+    }
+}
+
+const QDateTime& HistoryTimeframeModel::start() const
+{
+    return m_start;
+}
+
+void HistoryTimeframeModel::setStart(const QDateTime& start)
+{
+    if (start != m_start) {
+        m_start = start;
+        invalidateFilter();
+        Q_EMIT startChanged();
+    }
+}
+
+const QDateTime& HistoryTimeframeModel::end() const
+{
+    return m_end;
+}
+
+void HistoryTimeframeModel::setEnd(const QDateTime& end)
+{
+    if (end != m_end) {
+        m_end = end;
+        invalidateFilter();
+        Q_EMIT endChanged();
+    }
+}
+
+bool HistoryTimeframeModel::filterAcceptsRow(int source_row, const QModelIndex& source_parent) const
+{
+    QModelIndex index = sourceModel()->index(source_row, 0, source_parent);
+    QDateTime lastVisit = sourceModel()->data(index, HistoryModel::LastVisit).toDateTime();
+    if (m_start.isValid() && (lastVisit < m_start)) {
+        return false;
+    }
+    if (m_end.isValid() && (lastVisit > m_end)) {
+        return false;
+    }
+    return true;
+}
---
+++ src/Ubuntu/Browser/history-timeframe-model.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2013 Canonical Ltd.
+ *
+ * This file is part of webbrowser-app.
+ *
+ * webbrowser-app is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * webbrowser-app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __HISTORY_TIMEFRAME_MODEL_H__
+#define __HISTORY_TIMEFRAME_MODEL_H__
+
+// Qt
+#include <QtCore/QDateTime>
+#include <QtCore/QSortFilterProxyModel>
+
+class HistoryModel;
+
+class HistoryTimeframeModel : public QSortFilterProxyModel
+{
+    Q_OBJECT
+
+    Q_PROPERTY(HistoryModel* sourceModel READ sourceModel WRITE setSourceModel NOTIFY sourceModelChanged)
+    Q_PROPERTY(QDateTime start READ start WRITE setStart NOTIFY startChanged)
+    Q_PROPERTY(QDateTime end READ end WRITE setEnd NOTIFY endChanged)
+
+public:
+    HistoryTimeframeModel(QObject* parent=0);
+
+    HistoryModel* sourceModel() const;
+    void setSourceModel(HistoryModel* sourceModel);
+
+    const QDateTime& start() const;
+    void setStart(const QDateTime& start);
+
+    const QDateTime& end() const;
+    void setEnd(const QDateTime& end);
+
+Q_SIGNALS:
+    void sourceModelChanged() const;
+    void startChanged() const;
+    void endChanged() const;
+
+protected:
+    // reimplemented from QSortFilterProxyModel
+    bool filterAcceptsRow(int source_row, const QModelIndex& source_parent) const;
+
+private:
+    QDateTime m_start;
+    QDateTime m_end;
+};
+
+#endif // __HISTORY_TIMEFRAME_MODEL_H__
