942a348b287abfba52da6fabafe49b02a19585db
Ugo Riboni <ugo.riboni@canonical.com>
2013-11-27 12:40:50 +0100

    Disable a test dependent on an autopilot bug, and improve the integration test

--- tests/autopilot/webbrowser_app/tests/test_content_pick.py
+++ tests/autopilot/webbrowser_app/tests/test_content_pick.py
@@ -8,6 +8,8 @@
 
 from __future__ import absolute_import
 
+from autopilot.platform import model
+from testtools import skipIf, skip
 from webbrowser_app.tests import StartOpenRemotePageTestCaseBase
 from unity8 import process_helpers as helpers
 from autopilot.introspection import get_proxy_object_for_existing_process
@@ -15,9 +17,10 @@ from autopilot.matchers import Eventually
 from testtools.matchers import Equals
 import os
 
+@skip("Will not work until bug #1255077 is solved")
 class TestContentPick(StartOpenRemotePageTestCaseBase):
 
-    """Tests that picking content works properly."""
+    """Tests that content picking dialog show up."""
 
     def test_pick_image(self):
         url = self.base_url + "/uploadform"
@@ -28,13 +31,16 @@ class TestContentPick(StartOpenRemotePageTestCaseBase):
 
         # I don't know how to wait until the dialog is up,
         # because I can't access the dialog ;)
-        self.main_window.print_tree("/tmp/tree")
+        # TODO: access the ContentPickerDialog here
 
-#@skipIf(model() == 'Desktop', "Phablet only")
+@skipIf(model() == 'Desktop', "Phablet only")
 class TestContentPickerIntegration(StartOpenRemotePageTestCaseBase):
+
+    """Tests that the gallery app is brought up to choose image content"""
 
     def tearDown(self):
         os.system("pkill gallery-app")
+        os.system("pkill webbrowser-app")
         super(StartOpenRemotePageTestCaseBase, self).tearDown()
 
     def get_unity8_proxy_object(self):
@@ -52,11 +58,11 @@ class TestContentPickerIntegration(StartOpenRemotePageTestCaseBase):
         self.pointing_device.click_object(webview)
 
         unity8 = self.get_unity8_proxy_object()
+        shell = unity8.select_single("Shell")
         self.assertThat(
             lambda: self.get_current_focused_appid(unity8),
             Eventually(Equals("gallery-app"))
         )
 
-        self.main_window.print_tree("/tmp/tree")
 
 
