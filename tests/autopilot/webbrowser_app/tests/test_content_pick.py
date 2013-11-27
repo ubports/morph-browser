d40e3b9a6a347f0d6b4d5d5aed92844fd450a562
Ugo Riboni <ugo.riboni@canonical.com>
2013-11-27 17:07:58 +0100

    Try interacting with the gallery app

--- tests/autopilot/webbrowser_app/tests/test_content_pick.py
+++ tests/autopilot/webbrowser_app/tests/test_content_pick.py
@@ -8,14 +8,15 @@
 
 from __future__ import absolute_import
 
+from autopilot.introspection import get_proxy_object_for_existing_process
 from autopilot.platform import model
+from autopilot.matchers import Eventually
+from testtools.matchers import Equals, NotEquals
 from testtools import skipIf, skip
 from webbrowser_app.tests import StartOpenRemotePageTestCaseBase
 from unity8 import process_helpers as helpers
-from autopilot.introspection import get_proxy_object_for_existing_process
-from autopilot.matchers import Eventually
-from testtools.matchers import Equals
-import os
+from ubuntuuitoolkit import emulators as toolkit_emulators
+import os, subprocess
 
 @skip("Will not work until bug #1255077 is solved")
 class TestContentPick(StartOpenRemotePageTestCaseBase):
@@ -50,6 +51,22 @@ class TestContentPickerIntegration(StartOpenRemotePageTestCaseBase):
     def get_current_focused_appid(self, unity8):
         return unity8.select_single("Shell").currentFocusedAppId
 
+    def set_testability_environment_variable(self):
+        """Makes sure every app opened in the environment loads the
+        testability driver."""
+
+        subprocess.check_call([
+            "/sbin/initctl",
+            "set-env",
+            "QT_LOAD_TESTABILITY=1"
+        ])
+
+    def get_app_pid(self, app):
+        try:
+            return int(subprocess.check_output(["pidof", app]).strip())
+        except CalledProcessError:
+            return -1
+
     def test_image_picker_is_gallery(self):
         url = self.base_url + "/uploadform"
         self.go_to_url(url)
@@ -63,6 +80,24 @@ class TestContentPickerIntegration(StartOpenRemotePageTestCaseBase):
             lambda: self.get_current_focused_appid(unity8),
             Eventually(Equals("gallery-app"))
         )
+
+    def test_image_picker_pick_image(self):
+        self.set_testability_environment_variable()
+        self.test_image_picker_is_gallery()
+
+        unity8 = self.get_unity8_proxy_object()
+        self.assertThat(lambda: self.get_app_pid("gallery-app"), Eventually(NotEquals(-1)))
+
+        gallery_proxy = get_proxy_object_for_existing_process(
+            self.get_app_pid("gallery-app"),
+            emulator_base = toolkit_emulators.UbuntuUIToolkitEmulatorBase
+        )
+
+        print gallery_proxy
+        print gallery_proxy.wait_select_single("QQuickView") # This works
+        print gallery_proxy.wait_select_single("Loader", objectName="pickLoader") # This doesn't
+        print gallery_proxy.wait_select_single("PickerMainView", objectName="pickerMainView") # This doesn't too
+        print gallery_proxy.wait_select_single("MainView", objectName="pickerMainView") # This doesn't too
 
 
 
