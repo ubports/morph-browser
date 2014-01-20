eef03a6435676204181cb292d571b6f809416430
Olivier Tilloy <olivier.tilloy@canonical.com>
2014-01-20 22:17:55 +0100

    Unset property before deleting its value.

--- src/app/browserapplication.cpp
+++ src/app/browserapplication.cpp
@@ -41,6 +41,7 @@ BrowserApplication::BrowserApplication(int& argc, char** argv)
 
 BrowserApplication::~BrowserApplication()
 {
+    m_webbrowserWindowProxy->setWindow(NULL);
     delete m_window;
     delete m_webbrowserWindowProxy;
     delete m_component;
