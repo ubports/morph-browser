01fb307506ab85fdd9d3c749a23a042d56d88894
Olivier Tilloy <olivier.tilloy@canonical.com>
2014-03-17 13:44:43 +0100

    Clean up build dependencies.

--- tests/unittests/qml/tst_QmlTests.cpp
+++ tests/unittests/qml/tst_QmlTests.cpp
@@ -16,6 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <QtQuickTest/quicktest.h>
+#include <QtQuickTest/QtQuickTest>
 QUICK_TEST_MAIN(QmlTests)
 
