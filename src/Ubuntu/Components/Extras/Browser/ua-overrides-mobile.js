6db60d1fffca05c9593a01b9089f1b70da8f37d5
Olivier Tilloy <olivier.tilloy@canonical.com>
2014-04-14 10:12:30 +0200

    Escape literal dots in UA override matching regular expressions.

--- src/Ubuntu/Components/Extras/Browser/ua-overrides-mobile.js
+++ src/Ubuntu/Components/Extras/Browser/ua-overrides-mobile.js
@@ -19,5 +19,5 @@
 .pragma library
 
 var overrides = [
-    [/^https?:\/\/mail.google.com\//, "Mozilla/5.0 (Linux; Ubuntu 14.04 like Android 4.4) AppleWebKit/537.36 Chromium/35.0.1870.2 Mobile Safari"],
+    [/^https?:\/\/mail\.google\.com\//, "Mozilla/5.0 (Linux; Ubuntu 14.04 like Android 4.4) AppleWebKit/537.36 Chromium/35.0.1870.2 Mobile Safari"],
 ];
