debf42319c4b1e4490956851d76dff3d598df50b
Alexandre Abreu <alexandre.abreu@canonical.com>
2014-07-30 09:33:00 -0400

    nits

--- src/Ubuntu/Web/UbuntuWebView02.qml
+++ src/Ubuntu/Web/UbuntuWebView02.qml
@@ -301,4 +301,3 @@ Oxide.WebView {
         }
     }
 }
-
--- src/app/webcontainer/webapp-container.cpp
+++ src/app/webcontainer/webapp-container.cpp
@@ -186,7 +186,7 @@ void WebappContainer::printUsage() const
     out << "  --fullscreen                        display full screen" << endl;
     out << "  --local-webapp-manifest             configure the webapp assuming that it has a local manifest.json file" << endl;
     out << "  --maximized                         opens the application maximized" << endl;
-    out << "  --inspector[=PORT]                  run a remote inspector on a specified port or" << REMOTE_INSPECTOR_PORT << "as the default port" << endl;
+    out << "  --inspector[=PORT]                  run a remote inspector on a specified port or " << REMOTE_INSPECTOR_PORT << " as the default port" << endl;
     out << "  --app-id=APP_ID                     run the application with a specific APP_ID" << endl;
     out << "  --homepage=URL                      override any URL passed as an argument" << endl;
     out << "  --webapp=name                       try to match the webapp by name with an installed integration script" << endl;
