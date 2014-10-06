f666fd2c8f08027a19a4554eaf6867649b6c3b1c
Alexandre Abreu <alexandre.abreu@canonical.com>
2014-10-06 09:05:06 -0400

    update

--- src/app/MimeTypeMapper.js
+++ src/app/MimeTypeMapper.js
@@ -26,9 +26,8 @@ function mimeTypeToContentType(mimeType) {
         return ContentType.Videos;
     } else if(mimeType.search("text/x-vcard") === 0) {
         return ContentType.Contacts;
-    } else if(mimeType.search("text/") === 0) {
-        return ContentType.Documents;
-    } else if(mimeType.search("application/pdf") === 0
+    } else if(mimeType.search("text/") === 0
+              || mimeType.search("application/pdf") === 0
               || mimeType.search("application/x-pdf") === 0
               || mimeType.search("application/vnd.pdf") === 0) {
         return ContentType.Documents;
