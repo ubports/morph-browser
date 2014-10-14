7d7a5a26db2c77ea80183eebd718acf3ffd1d3f5
Olivier Tilloy <olivier.tilloy@canonical.com>
2014-10-14 11:59:53 +0200

    Add a UA override rule for HSBC’s Brazilian mobile site.

--- src/Ubuntu/Web/ua-overrides-mobile.js
+++ src/Ubuntu/Web/ua-overrides-mobile.js
@@ -22,4 +22,5 @@ var overrides = [
     [/^https?:\/\/mail\.google\.com\//, "Mozilla/5.0 (Linux; Ubuntu 14.04 like Android 4.4) AppleWebKit/537.36 Chromium/35.0.1870.2 Mobile Safari"],
     [/^https?:\/\/m\.youtube\.com\//, "Mozilla/5.0 (Linux; Ubuntu 14.04 like Android 4.4;) AppleWebKit/537.36 Chromium/35.0.1870.2 Mobile Safari/537.36"], // http://pad.lv/1228415
     [/^http:\/\/chrome\.angrybirds\.com\//, "Mozilla/5.0 (Linux; Ubuntu 14.04 like Android 4.4;) AppleWebKit/537.36 Chrome/35.0.1870.2 Mobile Safari/537.36"], // http://pad.lv/1284158
+    [/^https?:\/\/(\w+\.)*hsbc\.com\.br\//, "Mozilla/5.0 (Linux; Ubuntu 14.04 like Android 4.4;) AppleWebKit/537.36 Chrome/35.0.1870.2 Mobile Safari/537.36"], // http://pad.lv/1380657
 ];
