4d8308fd6a6b4ceb81422e9c90967c05844bdd5b
Alexandre Abreu <alexandre.abreu@canonical.com>
2015-03-24 09:21:48 -0400

    flake8

--- tests/autopilot/webapp_container/tests/test_popup_webview_overlay.py
+++ tests/autopilot/webapp_container/tests/test_popup_webview_overlay.py
@@ -13,8 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import time
-
 from testtools.matchers import Equals, Contains, GreaterThan
 from autopilot.matchers import Eventually
 
