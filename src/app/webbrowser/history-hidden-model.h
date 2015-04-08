4a39f3e442aa9a355967445927531fa7cfcce871
Arthur Mello <arthur.mello@canonical.com>
2015-04-08 15:35:02 -0300

    Add a proxy model to remove hidden entries

---
+++ src/app/webbrowser/history-hidden-model.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015 Canonical Ltd.
+ *
+ * This file is part of webbrowser-app.
+ *
+ * webbrowser-app is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * webbrowser-app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __HISTORY_HIDDEN_MODEL_H__
+#define __HISTORY_HIDDEN_MODEL_H__
+
+// Qt
+#include <QtCore/QSortFilterProxyModel>
+
+class HistoryByVisitsModel;
+
+class HistoryHiddenModel : public QSortFilterProxyModel
+{
+    Q_OBJECT
+
+    Q_PROPERTY(HistoryByVisitsModel* sourceModel READ sourceModel WRITE setSourceModel NOTIFY sourceModelChanged)
+
+public:
+    HistoryHiddenModel(QObject* parent=0);
+
+    HistoryByVisitsModel* sourceModel() const;
+    void setSourceModel(HistoryByVisitsModel* sourceModel);
+
+Q_SIGNALS:
+    void sourceModelChanged() const;
+
+protected:
+    // reimplemented from QSortFilterProxyModel
+    bool filterAcceptsRow(int source_row, const QModelIndex& source_parent) const;
+};
+
+#endif // __HISTORY_HIDDEN_MODEL_H__
