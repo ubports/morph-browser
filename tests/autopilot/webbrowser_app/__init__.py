99afc73adbeb75f21f8f97d5c9d2ac6c1e26c531
Leo Arias <leo.arias@canonical.com>
2015-05-06 16:33:59 -0600

    Moved the application custom proxy object to the top of the namespace.

--- tests/autopilot/webbrowser_app/__init__.py
+++ tests/autopilot/webbrowser_app/__init__.py
@@ -1,6 +1,6 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 #
-# Copyright 2013 Canonical
+# Copyright 2013-2015 Canonical
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 3, as published
@@ -15,3 +15,27 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """webbrowser-app autopilot tests and emulators - top level package."""
+
+
+import ubuntuuitoolkit as uitk
+
+from autopilot import introspection
+
+from webbrowser_app.emulators import browser
+
+
+class Webbrowser(uitk.UbuntuUIToolkitCustomProxyObjectBase):
+
+    """Autopilot custom proxy object for the webbrowser app."""
+
+    @classmethod
+    def validate_dbus_object(cls, path, state):
+        name = introspection.get_classname_from_path(path)
+        if name == b'webbrowser-app':
+            if state['applicationName'][1] == 'webbrowser-app':
+                return True
+        return False
+
+    @property
+    def main_window(self):
+        return self.select_single(browser.Browser)
