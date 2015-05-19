ac8e92b737ddbae472a9bb879b69abdd4eb1c688
Arthur Mello <arthur.mello@canonical.com>
2015-05-19 11:33:27 -0300

    Add missing import

--- tests/autopilot/webbrowser_app/tests/test_private.py
+++ tests/autopilot/webbrowser_app/tests/test_private.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from testtools.matchers import Equals
+from testtools.matchers import Equals, NotEquals
 from autopilot.matchers import Eventually
 
 from webbrowser_app.tests import StartOpenRemotePageTestCaseBase
