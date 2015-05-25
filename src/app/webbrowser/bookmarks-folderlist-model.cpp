163a22cf6c435f32cc848bc3ac7ad8dd0d581e9f
Arthur Mello <arthur.mello@canonical.com>
2015-05-25 18:55:15 -0300

    Add bookmarks folder list model

---
+++ src/app/webbrowser/bookmarks-folderlist-model.cpp
@@ -0,0 +1,216 @@
+/*
+ * Copyright 2015 Canonical Ltd.
+ *
+ * This file is part of webbrowser-app.
+ *
+ * webbrowser-app is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * webbrowser-app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "bookmarks-folder-model.h"
+#include "bookmarks-folderlist-model.h"
+#include "bookmarks-model.h"
+
+// Qt
+#include <QtCore/QStringList>
+
+/*!
+    \class BookmarksFolderListModel
+    \brief List model that exposes bookmarks entries grouped by folder name
+
+    BookmarksFolderListModel is a list model that exposes bookmarks entries
+    from a BookmarksModel grouped by folder name. Each item in the list has
+    three roles: 'folder' for the folder name, 'lastAddition' for the timestamp
+    of the last page bookmarked in this folder, and 'entries' for the
+    corresponding BookmarksFolderModel that contains all entries in this group.
+*/
+BookmarksFolderListModel::BookmarksFolderListModel(QObject* parent)
+    : QAbstractListModel(parent)
+    , m_sourceModel(0)
+{
+}
+
+BookmarksFolderListModel::~BookmarksFolderListModel()
+{
+    clearFolders();
+}
+
+QHash<int, QByteArray> BookmarksFolderListModel::roleNames() const
+{
+    static QHash<int, QByteArray> roles;
+    if (roles.isEmpty()) {
+        roles[Folder] = "folder";
+        roles[LastAddition] = "lastAddition";
+        roles[LastAdditionDate] = "lastAdditionDate";
+        roles[Entries] = "entries";
+    }
+    return roles;
+}
+
+int BookmarksFolderListModel::rowCount(const QModelIndex& parent) const
+{
+    Q_UNUSED(parent);
+    return m_folders.count();
+}
+
+QVariant BookmarksFolderListModel::data(const QModelIndex& index, int role) const
+{
+    if (!index.isValid()) {
+        return QVariant();
+    }
+    const QString folder = m_folders.keys().at(index.row());
+    BookmarksFolderModel* entries = m_folders.value(folder);
+
+    switch (role) {
+    case Folder:
+        return folder;
+    case LastAddition:
+        return entries->lastAddition();
+    case LastAdditionDate:
+        return entries->lastAddition().toLocalTime().date();
+    case Entries:
+        return QVariant::fromValue(entries);
+    default:
+        return QVariant();
+    }
+}
+
+BookmarksModel* BookmarksFolderListModel::sourceModel() const
+{
+    return m_sourceModel;
+}
+
+void BookmarksFolderListModel::setSourceModel(BookmarksModel* sourceModel)
+{
+    if (sourceModel != m_sourceModel) {
+        beginResetModel();
+        if (m_sourceModel != 0) {
+            m_sourceModel->disconnect(this);
+        }
+        clearFolders();
+        m_sourceModel = sourceModel;
+        populateModel();
+        if (m_sourceModel != 0) {
+            connect(m_sourceModel, SIGNAL(rowsInserted(const QModelIndex&, int, int)),
+                    SLOT(onRowsInserted(const QModelIndex&, int, int)));
+            connect(m_sourceModel, SIGNAL(modelReset()), SLOT(onModelReset()));
+            connect(m_sourceModel, SIGNAL(layoutChanged(QList<QPersistentModelIndex>, QAbstractItemModel::LayoutChangeHint)),
+                    SLOT(onModelReset()));
+        }
+        endResetModel();
+        Q_EMIT sourceModelChanged();
+    }
+}
+
+void BookmarksFolderListModel::clearFolders()
+{
+    Q_FOREACH(const QString& folder, m_folders.keys()) {
+        delete m_folders.take(folder);
+    }
+}
+
+void BookmarksFolderListModel::populateModel()
+{
+    if (m_sourceModel != 0) {
+        int count = m_sourceModel->rowCount();
+        for (int i = 0; i < count; ++i) {
+            QString folder = getFolderFromSourceModel(m_sourceModel->index(i, 0));
+            if (!m_folders.contains(folder)) {
+                insertNewFolder(folder);
+            }
+        }
+    }
+}
+
+void BookmarksFolderListModel::onRowsInserted(const QModelIndex& parent, int start, int end)
+{
+    for (int i = start; i <= end; ++i) {
+        QString folder = getFolderFromSourceModel(m_sourceModel->index(i, 0, parent));
+        if (!m_folders.contains(folder)) {
+            QStringList folders = m_folders.keys();
+            int insertAt = 0;
+            while (insertAt < folders.count()) {
+                if (folder.compare(folders.at(insertAt)) < 0) {
+                    break;
+                }
+                ++insertAt;
+            }
+            beginInsertRows(QModelIndex(), insertAt, insertAt);
+            insertNewFolder(folder);
+            endInsertRows();
+        }
+    }
+}
+
+void BookmarksFolderListModel::onModelReset()
+{
+    beginResetModel();
+    clearFolders();
+    populateModel();
+    endResetModel();
+}
+
+void BookmarksFolderListModel::insertNewFolder(const QString& folder)
+{
+    BookmarksFolderModel* model = new BookmarksFolderModel(this);
+    model->setSourceModel(m_sourceModel);
+    model->setFolder(folder);
+    connect(model, SIGNAL(rowsInserted(QModelIndex, int, int)), SLOT(onFolderDataChanged()));
+    connect(model, SIGNAL(rowsRemoved(QModelIndex, int, int)), SLOT(onFolderRowsRemoved(QModelIndex, int, int)));
+    connect(model, SIGNAL(rowsMoved(QModelIndex, int, int, QModelIndex, int)), SLOT(onFolderDataChanged()));
+    connect(model, SIGNAL(layoutChanged(QList<QPersistentModelIndex>, QAbstractItemModel::LayoutChangeHint)), SLOT(onFolderDataChanged()));
+    connect(model, SIGNAL(dataChanged(QModelIndex, QModelIndex)), SLOT(onFolderDataChanged()));
+    connect(model, SIGNAL(modelReset()), SLOT(onFolderDataChanged()));
+    connect(model, SIGNAL(lastAdditionChanged()), SLOT(onFolderDataChanged()));
+    m_folders.insert(folder, model);
+}
+
+QString BookmarksFolderListModel::getFolderFromSourceModel(const QModelIndex& index) const
+{
+    return m_sourceModel->data(index, BookmarksModel::Folder).toString();
+}
+
+void BookmarksFolderListModel::onFolderRowsRemoved(const QModelIndex& parent, int start, int end)
+{
+    Q_UNUSED(parent);
+    Q_UNUSED(start);
+    Q_UNUSED(end);
+    BookmarksFolderModel* model = qobject_cast<BookmarksFolderModel*>(sender());
+    if (model != 0) {
+        const QString& folder = model->folder();
+        if (model->rowCount() == 0) {
+            int removeAt = m_folders.keys().indexOf(folder);
+            beginRemoveRows(QModelIndex(), removeAt, removeAt);
+            delete m_folders.take(folder);
+            endRemoveRows();
+        } else {
+            emitDataChanged(folder);
+        }
+    }
+}
+
+void BookmarksFolderListModel::onFolderDataChanged()
+{
+    BookmarksFolderModel* model = qobject_cast<BookmarksFolderModel*>(sender());
+    if (model != 0) {
+        emitDataChanged(model->folder());
+    }
+}
+
+void BookmarksFolderListModel::emitDataChanged(const QString& folder)
+{
+    int i = m_folders.keys().indexOf(folder);
+    if (i != -1) {
+        QModelIndex index = this->index(i, 0);
+        Q_EMIT dataChanged(index, index, QVector<int>() << LastAddition << LastAdditionDate << Entries);
+    }
+}
---
+++ src/app/webbrowser/bookmarks-folderlist-model.h
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2015 Canonical Ltd.
+ *
+ * This file is part of webbrowser-app.
+ *
+ * webbrowser-app is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * webbrowser-app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __BOOKMARKS_FOLDERLIST_MODEL_H__
+#define __BOOKMARKS_FOLDERLIST_MODEL_H__
+
+// Qt
+#include <QtCore/QAbstractListModel>
+#include <QtCore/QMap>
+#include <QtCore/QString>
+
+class BookmarksFolderModel;
+class BookmarksModel;
+
+class BookmarksFolderListModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+    Q_PROPERTY(BookmarksModel* sourceModel READ sourceModel WRITE setSourceModel NOTIFY sourceModelChanged)
+
+    Q_ENUMS(Roles)
+
+public:
+    BookmarksFolderListModel(QObject* parent=0);
+    ~BookmarksFolderListModel();
+
+    enum Roles {
+        Folder = Qt::UserRole + 1,
+        LastAddition,
+        LastAdditionDate,
+        Entries
+    };
+
+    // reimplemented from QAbstractListModel
+    QHash<int, QByteArray> roleNames() const;
+    int rowCount(const QModelIndex& parent=QModelIndex()) const;
+    QVariant data(const QModelIndex& index, int role) const;
+
+    BookmarksModel* sourceModel() const;
+    void setSourceModel(BookmarksModel* sourceModel);
+
+Q_SIGNALS:
+    void sourceModelChanged() const;
+
+private Q_SLOTS:
+    void onRowsInserted(const QModelIndex& parent, int start, int end);
+    void onModelReset();
+
+    void onFolderRowsRemoved(const QModelIndex& parent, int start, int end);
+    void onFolderDataChanged();
+
+private:
+    BookmarksModel* m_sourceModel;
+    QMap<QString, BookmarksFolderModel*> m_folders;
+
+    void clearFolders();
+    void populateModel();
+    void insertNewFolder(const QString& folder);
+    QString getFolderFromSourceModel(const QModelIndex& index) const;
+    void emitDataChanged(const QString& domain);
+};
+
+#endif // __BOOKMARKS_FOLDERLIST_MODEL_H__
