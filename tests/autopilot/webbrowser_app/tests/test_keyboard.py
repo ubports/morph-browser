f327c313fa92e50e90e255b51d04cb33987e8b7e
Arthur Mello <arthur.mello@canonical.com>
2015-08-11 17:57:58 -0300

    Fixing check for autofocus

--- tests/autopilot/webbrowser_app/tests/test_keyboard.py
+++ tests/autopilot/webbrowser_app/tests/test_keyboard.py
@@ -283,7 +283,9 @@ class TestKeyboard(PrepopulatedDatabaseTestCaseBase):
         self.assertThat(self.main_window.get_history_view(), Equals(None))
         self.open_history()
         history_view = self.main_window.get_history_view()
-        self.assertThat(history_view.activeFocus, Eventually(Equals(True)))
+
+        if not self.main_window.wide:
+            self.assertThat(history_view.activeFocus, Eventually(Equals(True)))
 
         self.main_window.press_key('Escape')
         history_view.wait_until_destroyed()
