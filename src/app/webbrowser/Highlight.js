0b53ae9d6bb5c8a45fc98f5c59ecdedc74c2a961
Ugo Riboni <ugo.riboni@canonical.com>
2015-09-16 18:15:51 +0200

    Move internal highlight functions and variables within the scope of the public one

--- src/app/webbrowser/Highlight.js
+++ src/app/webbrowser/Highlight.js
@@ -16,22 +16,22 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-var termsRe
-var highlight = '<font color="#752571">$&</font>'
-var searchTerms = []
-
-function escapeTerm(term) {
-    // Escape special characters in a search term
-    // (a simpler version of preg_quote).
-    return term.replace(/[().?+|*^$]/g, '\\$&')
-}
-
-function setSearchTerms(terms) {
-    searchTerms = terms
-    termsRe = new RegExp(terms.map(escapeTerm).join('|'), 'ig')
-}
-
 function highlightTerms(text, terms) {
+    var termsRe
+    var highlight = '<font color="#752571">$&</font>'
+    var searchTerms = []
+
+    function escapeTerm(term) {
+        // Escape special characters in a search term
+        // (a simpler version of preg_quote).
+        return term.replace(/[().?+|*^$]/g, '\\$&')
+    }
+
+    function setSearchTerms(terms) {
+        searchTerms = terms
+        termsRe = new RegExp(terms.map(escapeTerm).join('|'), 'ig')
+    }
+
     // Highlight the matching terms in a case-insensitive manner
     if (text && text.toString()) {
         if (searchTerms !== terms) setSearchTerms(terms)
