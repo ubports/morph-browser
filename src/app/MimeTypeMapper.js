eb97516d53820bdd4fb95a8ee38aee9eeedcb276
Olivier Tilloy <olivier.tilloy@canonical.com>
2015-12-11 16:01:38 +0100

    Also add support for openoffice and MS-office mime types to the code that converts them to a well-known ContentHub type.

--- src/app/MimeTypeMapper.js
+++ src/app/MimeTypeMapper.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Canonical Ltd.
+ * Copyright 2014-2015 Canonical Ltd.
  *
  * This file is part of webbrowser-app.
  *
@@ -18,27 +18,34 @@
 
 
 function mimeTypeToContentType(mimeType) {
-    if(mimeType.search("image/") === 0) {
-        return ContentType.Pictures;
-    } else if(mimeType.search("audio/") === 0) {
-        return ContentType.Music;
-    } else if(mimeType.search("video/") === 0) {
-        return ContentType.Videos;
-    } else if(mimeType.search("text/x-vcard") === 0
-              || mimeType.search("text/vcard") === 0) {
-        return ContentType.Contacts;
-    } else if(mimeType.search("application/epub[+]zip") === 0
-              || mimeType.search("application/vnd\.amazon\.ebook") === 0
-              || mimeType.search("application/x-mobipocket-ebook") === 0
-              || mimeType.search("application/x-fictionbook+xml") === 0
-              || mimeType.search("application/x-ms-reader") === 0) {
-        return ContentType.EBooks; 
-    } else if(mimeType.search("text/") === 0
-              || mimeType.search("application/pdf") === 0
-              || mimeType.search("application/x-pdf") === 0
-              || mimeType.search("application/vnd\.pdf") === 0) {
-        return ContentType.Documents;
+    if (mimeType.search("image/") === 0) {
+        return ContentType.Pictures
+    } else if (mimeType.search("audio/") === 0) {
+        return ContentType.Music
+    } else if (mimeType.search("video/") === 0) {
+        return ContentType.Videos
+    } else if (mimeType.search("text/x-vcard") === 0
+               || mimeType.search("text/vcard") === 0) {
+        return ContentType.Contacts
+    } else if (mimeType.search("application/epub[+]zip") === 0
+               || mimeType.search("application/vnd\.amazon\.ebook") === 0
+               || mimeType.search("application/x-mobipocket-ebook") === 0
+               || mimeType.search("application/x-fictionbook+xml") === 0
+               || mimeType.search("application/x-ms-reader") === 0) {
+        return ContentType.EBooks
+    } else if (mimeType.search("text/") === 0
+               || mimeType.search("application/pdf") === 0
+               || mimeType.search("application/x-pdf") === 0
+               || mimeType.search("application/vnd\.pdf") === 0
+               || mimeType.search("application/vnd\.oasis\.opendocument") === 0
+               || mimeType.search("application/msword") === 0
+               || mimeType.search("application/vnd\.openxmlformats-officedocument\.wordprocessingml\.document") === 0
+               || mimeType.search("application/vnd\.openxmlformats-officedocument\.spreadsheetml\.sheet") === 0
+               || mimeType.search("application/vnd\.openxmlformats-officedocument\.presentationml\.presentation") === 0
+               || mimeType.search("application/vnd\.ms-excel") === 0
+               || mimeType.search("application/vnd\.ms-powerpoint") === 0) {
+        return ContentType.Documents
     } else {
-        return ContentType.Unknown;
+        return ContentType.Unknown
     }
 }
