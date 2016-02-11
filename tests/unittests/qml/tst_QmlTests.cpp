077ae96aa4c66d586ab1ce00f8753fae3d969a16
Olivier Tilloy <olivier.tilloy@canonical.com>
2016-02-11 09:26:47 +0100

    Fix unit tests.

--- tests/unittests/qml/tst_QmlTests.cpp
+++ tests/unittests/qml/tst_QmlTests.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2015 Canonical Ltd.
+ * Copyright 2013-2016 Canonical Ltd.
  *
  * This file is part of webbrowser-app.
  *
@@ -37,13 +37,7 @@
 #include "searchengine.h"
 #include "tabs-model.h"
 #include "text-search-filter-model.h"
-
-static QObject* FileOperations_singleton_factory(QQmlEngine* engine, QJSEngine* scriptEngine)
-{
-    Q_UNUSED(engine);
-    Q_UNUSED(scriptEngine);
-    return new FileOperations();
-}
+#include "Unity/InputInfo/qdeclarativeinputdevicemodel_p.h"
 
 class TestContext : public QObject
 {
@@ -126,60 +120,51 @@ private:
 };
 
 class HistoryModelMock : public HistoryModel {
-  Q_OBJECT
+    Q_OBJECT
 
 public:
-  static bool compareHistoryEntries(const HistoryEntry& a, const HistoryEntry& b) {
-      return a.lastVisit < b.lastVisit;
-  }
+    static bool compareHistoryEntries(const HistoryEntry& a, const HistoryEntry& b) {
+        return a.lastVisit < b.lastVisit;
+    }
 
-  Q_INVOKABLE int addByDate(const QUrl& url, const QString& title, const QDateTime& date)
-  {
-      int index = getEntryIndex(url);
-      int visitsToAdd = 1;
-      if (index == -1) {
-          add(url, title, QString());
-          index = getEntryIndex(url);
-          visitsToAdd = 0;
-      }
+    Q_INVOKABLE int addByDate(const QUrl& url, const QString& title, const QDateTime& date)
+    {
+        int index = getEntryIndex(url);
+        int visitsToAdd = 1;
+        if (index == -1) {
+            add(url, title, QString());
+            index = getEntryIndex(url);
+            visitsToAdd = 0;
+        }
 
-      // Since this is useful only for testing and efficiency is not critical
-      // we reorder the model and reset it every time we add a new item by date
-      // to keep things simple.
-      beginResetModel();
-      HistoryEntry entry = m_entries.takeAt(index);
-      entry.lastVisit = date;
-      entry.visits = entry.visits + visitsToAdd;
-      m_entries.append(entry);
-      std::sort(m_entries.begin(), m_entries.end(), compareHistoryEntries);
-      endResetModel();
+        // Since this is useful only for testing and efficiency is not critical
+        // we reorder the model and reset it every time we add a new item by date
+        // to keep things simple.
+        beginResetModel();
+        HistoryEntry entry = m_entries.takeAt(index);
+        entry.lastVisit = date;
+        entry.visits = entry.visits + visitsToAdd;
+        m_entries.append(entry);
+        std::sort(m_entries.begin(), m_entries.end(), compareHistoryEntries);
+        endResetModel();
 
-      updateExistingEntryInDatabase(entry);
+        updateExistingEntryInDatabase(entry);
 
-      return entry.visits;
-  }
+        return entry.visits;
+    }
 };
 
-static QObject* TestContext_singleton_factory(QQmlEngine* engine, QJSEngine* scriptEngine)
-{
-    Q_UNUSED(engine);
-    Q_UNUSED(scriptEngine);
-    return new TestContext();
-}
+#define MAKE_SINGLETON_FACTORY(type) \
+    static QObject* type##_singleton_factory(QQmlEngine* engine, QJSEngine* scriptEngine) { \
+        Q_UNUSED(engine); \
+        Q_UNUSED(scriptEngine); \
+        return new type(); \
+    }
 
-static QObject* BookmarksModel_singleton_factory(QQmlEngine* engine, QJSEngine* scriptEngine)
-{
-    Q_UNUSED(engine);
-    Q_UNUSED(scriptEngine);
-    return new BookmarksModel();
-}
-
-static QObject* HistoryModel_singleton_factory(QQmlEngine* engine, QJSEngine* scriptEngine)
-{
-    Q_UNUSED(engine);
-    Q_UNUSED(scriptEngine);
-    return new HistoryModelMock();
-}
+MAKE_SINGLETON_FACTORY(FileOperations)
+MAKE_SINGLETON_FACTORY(BookmarksModel)
+MAKE_SINGLETON_FACTORY(HistoryModelMock)
+MAKE_SINGLETON_FACTORY(TestContext)
 
 int main(int argc, char** argv)
 {
@@ -191,7 +176,7 @@ int main(int argc, char** argv)
     qmlRegisterType<TabsModel>(browserUri, 0, 1, "TabsModel");
     qmlRegisterSingletonType<BookmarksModel>(browserUri, 0, 1, "BookmarksModel", BookmarksModel_singleton_factory);
     qmlRegisterType<BookmarksFolderListModel>(browserUri, 0, 1, "BookmarksFolderListModel");
-    qmlRegisterSingletonType<HistoryModel>(browserUri, 0, 1, "HistoryModel", HistoryModel_singleton_factory);
+    qmlRegisterSingletonType<HistoryModel>(browserUri, 0, 1, "HistoryModel", HistoryModelMock_singleton_factory);
     qmlRegisterType<HistoryDomainModel>(browserUri, 0, 1, "HistoryDomainModel");
     qmlRegisterType<HistoryDomainListModel>(browserUri, 0, 1, "HistoryDomainListModel");
     qmlRegisterType<HistoryLastVisitDateListModel>(browserUri, 0, 1, "HistoryLastVisitDateListModel");
@@ -201,6 +186,10 @@ int main(int argc, char** argv)
 
     const char* testUri = "webbrowsertest.private";
     qmlRegisterSingletonType<TestContext>(testUri, 0, 1, "TestContext", TestContext_singleton_factory);
+
+    const char* inputInfoUri = "Unity.InputInfo";
+    qmlRegisterType<QDeclarativeInputDeviceModel>(inputInfoUri, 0, 1, "InputDeviceModel");
+    qmlRegisterType<QInputDevice>(inputInfoUri, 0, 1, "InputInfo");
 
     return quick_test_main(argc, argv, "QmlTests", nullptr);
 }
