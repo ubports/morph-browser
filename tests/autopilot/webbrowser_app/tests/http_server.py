2e0e036be382323a7545b8184460d5800f10e6d0
Olivier Tilloy <olivier.tilloy@canonical.com>
2016-02-29 18:24:47 +0100

    Add some autopilot tests to verify that favicons are stored and updated in the history database.

--- tests/autopilot/webbrowser_app/tests/http_server.py
+++ tests/autopilot/webbrowser_app/tests/http_server.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2013-2015 Canonical
+# Copyright 2013-2016 Canonical
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 3, as published
@@ -215,6 +215,20 @@ class HTTPRequestHandler(http.BaseHTTPRequestHandler):
                 "}, function() { location.href = '/test1' } " +
                 ", function() { location.href = '/test2' })</script>"
             )
+        elif self.path == "/favicon":
+            self.send_response(200)
+            html = '<html><head><link rel="icon" type="image/png" '
+            html += 'href="/assets/icon1.png"></head>'
+            html += '<body>favicon</body></html>'
+            self.send_html(html)
+        elif self.path == "/changingfavicon":
+            self.send_response(200)
+            html = '<html><head><link id="favicon" rel="shortcut icon" '
+            html += 'type="image/png" href="icon0.png"></head><body><script>'
+            html += 'var i = 0; window.setInterval(function() {'
+            html += 'document.getElementById("favicon").href = ++i + '
+            html += '".png"; }, 1000);</script></body></html>'
+            self.send_html(html)
         else:
             self.send_error(404)
 
--- tests/autopilot/webbrowser_app/tests/test_history.py
+++ tests/autopilot/webbrowser_app/tests/test_history.py
@@ -1,6 +1,6 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 #
-# Copyright 2015 Canonical
+# Copyright 2015-2016 Canonical
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 3, as published
@@ -16,7 +16,7 @@
 
 import time
 
-from testtools.matchers import Equals
+from testtools.matchers import EndsWith, Equals, StartsWith
 from autopilot.matchers import Eventually
 
 from webbrowser_app.tests import StartOpenRemotePageTestCaseBase
@@ -25,7 +25,7 @@ from webbrowser_app.tests import StartOpenRemotePageTestCaseBase
 class TestHistory(StartOpenRemotePageTestCaseBase):
 
     def test_history_not_save_404(self):
-        url = self.base_url + "/404page"
+        url = self.base_url + "/notfound"
         self.main_window.go_to_url(url)
         self.main_window.wait_until_page_loaded(url)
 
@@ -40,29 +40,23 @@ class TestHistory(StartOpenRemotePageTestCaseBase):
         # We have domains with subsections only on mobiles.
         # On wide we take all the entries directly
         if self.main_window.wide:
-            delegates = history.get_entries()
-
             # 2 addresses: /test1 and /test2
             self.assertThat(lambda: len(history.get_entries()),
                             Eventually(Equals(2)))
-            self.assertThat(sorted([delegate.url for delegate in delegates]),
-                            Equals(sorted([self.url, url])))
+            self.assertThat([entry.url for entry in history.get_entries()],
+                            Equals([url, self.url]))
         else:
             # 1 domain: the local one
-            domain_entries = history.get_domain_entries()
             self.assertThat(lambda: len(history.get_domain_entries()),
                             Eventually(Equals(1)))
-
-            self.pointing_device.click_object(domain_entries[0])
+            self.pointing_device.click_object(history.get_domain_entries()[0])
             expanded_history = self.main_window.get_expanded_history_view()
 
             # 2 addresses: /test1 and /test2
             self.assertThat(lambda: len(expanded_history.get_entries()),
                             Eventually(Equals(2)))
-
-            delegates = expanded_history.get_entries()
-            self.assertThat(sorted([delegate.url for delegate in delegates]),
-                            Equals(sorted([self.url, url])))
+            self.assertThat([e.url for e in expanded_history.get_entries()],
+                            Equals([url, self.url]))
 
     def test_expanded_history_view_header_swallows_clicks(self):
         # Regression test for https://launchpad.net/bugs/1518904
@@ -78,3 +72,52 @@ class TestHistory(StartOpenRemotePageTestCaseBase):
         # There should be only one instance on the expanded history view.
         # If there’s more, the following call will raise an exception.
         self.main_window.get_expanded_history_view()
+
+    def test_favicon_saved(self):
+        # Regression test for https://launchpad.net/bugs/1549780
+        url = self.base_url + "/favicon"
+        self.main_window.go_to_url(url)
+        self.main_window.wait_until_page_loaded(url)
+        history = self.open_history()
+        if self.main_window.wide:
+            self.assertThat(lambda: len(history.get_entries()),
+                            Eventually(Equals(2)))
+            first = history.get_entries()[0]
+        else:
+            self.assertThat(lambda: len(history.get_domain_entries()),
+                            Eventually(Equals(1)))
+            self.pointing_device.click_object(history.get_domain_entries()[0])
+            expanded_history = self.main_window.get_expanded_history_view()
+            self.assertThat(lambda: len(expanded_history.get_entries()),
+                            Eventually(Equals(2)))
+            first = expanded_history.get_entries()[0]
+        self.assertThat(first.url, Equals(url))
+        favicon = self.base_url + "/assets/icon1.png"
+        self.assertThat(first.icon, Equals(favicon))
+
+    def test_favicon_updated(self):
+        # Verify that a page dynamically updating its favicon
+        # triggers an update in the history database.
+        url = self.base_url + "/changingfavicon"
+        self.main_window.go_to_url(url)
+        self.main_window.wait_until_page_loaded(url)
+        history = self.open_history()
+        if self.main_window.wide:
+            self.assertThat(lambda: len(history.get_entries()),
+                            Eventually(Equals(2)))
+            first = history.get_entries()[0]
+        else:
+            self.assertThat(lambda: len(history.get_domain_entries()),
+                            Eventually(Equals(1)))
+            self.pointing_device.click_object(history.get_domain_entries()[0])
+            expanded_history = self.main_window.get_expanded_history_view()
+            self.assertThat(lambda: len(expanded_history.get_entries()),
+                            Eventually(Equals(2)))
+            first = expanded_history.get_entries()[0]
+        indexes = set()
+        while len(indexes) < 3:
+            self.assertThat(first.url, Equals(url))
+            icon = first.icon
+            self.assertThat(icon, StartsWith(self.base_url))
+            self.assertThat(icon, EndsWith(".png"))
+            indexes.add(int(first.icon[(len(self.base_url)+1):-4]))
