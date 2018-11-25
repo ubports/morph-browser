b7c94278fff4deba134937fa436929b6e1d5554d
Chris Clime <chris.clime@gmx.net>
2018-11-25 12:26:59 +0100

    only reset the input method, if the last char in the commit string is a letter, space, or a single quote

--- src/app/input-method-handler.cpp
+++ src/app/input-method-handler.cpp
@@ -27,10 +27,15 @@ bool inputMethodHandler::eventFilter(QObject* obj, QEvent* event)
 
     //qDebug() << "input event, predit string: " << inputevent->preeditString() << " commit string: " << inputevent->commitString();
 
-    // if the last char of the commit string is a number, do nothing (reset would change the keyboard layout back to letter mode)
-    if (! inputevent->commitString().isEmpty() && inputevent->commitString()[inputevent->commitString().length() - 1].isDigit())
+    if (! inputevent->commitString().isEmpty() )
     {
-        return false;
+        QChar lastCharOfCommitString = inputevent->commitString()[inputevent->commitString().length() - 1];
+
+        // numbers and special chars do automatically commit, no reset done here as it would change the keyboard mode
+        if ( (lastCharOfCommitString != "'") && (lastCharOfCommitString != " ") && ! lastCharOfCommitString.isLetter() )
+        {
+            return false;
+        }
     }
 
     // check the request contains information about the text format
