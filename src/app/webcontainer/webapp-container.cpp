2827ebb429cc99b6fdb62db41ff34357e58e07b5
Chris Clime <chris.clime@gmx.net>
2020-06-28 19:49:47 +0200

    instead of Qt::AA_EnableHighDpiScaling use multiply the zoom factor with the additional scale factor that would normally be applied by automatic scaling
    the noticable change to the user is, that the max. zoom is no longer 500%, but (500% / scaleFactor), but all saved settings are untouched, because the scaleFactor is applied additionally to the zoom set by the user
    if the Qt scale will be used again in the future, the "screenScaleFactor" will be 1.0 and thus behave like before the change

--- src/app/webcontainer/webapp-container.cpp
+++ src/app/webcontainer/webapp-container.cpp
@@ -520,11 +520,13 @@ void WebappContainer::onNewInstanceLaunched(const QStringList& arguments) const
 int main(int argc, char** argv)
 {
     qputenv("QTWEBENGINE_DISABLE_SANDBOX","1");
-    qputenv("QT_WEBENGINE_DISABLE_GPU","1");
-    qputenv("QT_AUTO_SCREEN_SCALE_FACTOR", "true");
+    // disable gpu
+    if (qgetenv("QTWEBENGINE_CHROMIUM_FLAGS") == QString())
+    {
+        qputenv("QTWEBENGINE_CHROMIUM_FLAGS", "--disable-gpu");
+    }
 
     QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     WebappContainer app(argc, argv);
     if (app.initialize()) {
         return app.run();
